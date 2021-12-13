0ea2b2f57a7950f15ebc2113b0db28f45b2c43f9
Kevin Bequoye <beq.kevin@gmail.com>
2021-12-13 16:12:00 +0100

    13122021: Adding burger-animation

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,22 +1,22 @@
 "use strict";
 
 // assign variables
-const navBtn = document.querySelector(".burger-btn");
-const menu = document.querySelector(".nav");
+const navBtn = document.querySelector(".menu__btn");
+const menu = document.querySelector(".menu");
 const menuLinks = document.getElementsByClassName("menu__link");
 
 
 // add eventListener
 navBtn.addEventListener("click", () => {
     navBtn.classList.toggle("open");
-    menu.classList.toggle("nav--visible");
+    menu.classList.toggle("menu--visible");
 });
 
 
 // add or remove class in stylesheet
 for (const link of menuLinks) {
     link.addEventListener("click", () => {
-        menu.classList.remove("nav--visible");
+        menu.classList.remove("menu--visible");
         navBtn.classList.remove("open");
     });
 }
